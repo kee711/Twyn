9b604dd4a169c5cca829437c6ac2d5dae194b21d
kee711 <kee71164@gmail.com>
2025-07-31 07:49:54 +0900

    fix: force-dynamic

--- app/[locale]/(landing)/page.tsx
+++ app/[locale]/(landing)/page.tsx
@@ -1,12 +1,13 @@
 import HomeClient from './HomeClient';
 
-export async function generateStaticParams() {
-  return [
-    { locale: 'ko' },
-    { locale: 'en' }
-  ];
-}
+// export async function generateStaticParams() {
+//   return [
+//     { locale: 'ko' },
+//     { locale: 'en' }
+//   ];
+// }
+export const dynamic = 'force-dynamic';
 
-export default function Home({ params }: { params: { locale: string } }) {
+export default function Home() {
   return <HomeClient />;
 }
