c508c3c0899501cb09110bd13f68efc5c1c957e9
kee711 <kee71164@gmail.com>
2025-07-31 08:53:08 +0900

    fix: build error

--- app/[locale]/(landing)/page.tsx
+++ app/[locale]/(landing)/page.tsx
@@ -1,13 +1,5 @@
-'use client';
-
 import HomeClient from './HomeClient';
 
-// export async function generateStaticParams() {
-//   return [
-//     { locale: 'ko' },
-//     { locale: 'en' }
-//   ];
-// }
 export const dynamic = 'force-dynamic';
 
 export default function Home() {
